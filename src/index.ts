c822b2706012c41276e7aa07413c1c2b9ba8e731
PolinaBalanyeva <38326946+PolinaBalanyeva@users.noreply.github.com>
2022-09-24 00:00:15 +0900

    fixed time interval

--- src/index.ts
+++ src/index.ts
@@ -30,8 +30,8 @@ const client = new Client({
   ],
 });
 
-const wait = 60 * 0.2;
-const interval = 60 * 2;
+const wait = 60 * 1;
+const interval = 60 * 10;
 const processing = new Set();
 const writing = { id: [0, 0], speakers: [0, 0], intervalIDs: [0, 0] };
 const idJSON = new json('id');
